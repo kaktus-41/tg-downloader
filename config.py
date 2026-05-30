327a31d8244e981497309ee087fb6f1d4e1703f3
Abubakr <kaktus@MacBook-Air-Abubakr.local>
2026-05-31 03:54:16 +0500

    fix settings attribute name case

--- config.py
+++ config.py
@@ -7,6 +7,7 @@ class Settings:
     admin_id: int = int(os.getenv("ADMIN_ID", "0"))
     api_id: str = os.getenv("API_ID", "")
     api_hash: str = os.getenv("API_HASH", "")
-    local_bot_api_url: str = os.getenv("LOCAL_BOT_API_URL", "https://api.telegram.org")
+    local_bot_api_url: str = os.getenv("LOCAL_BOT_API_URL", "http://localhost:8081")
+    LOCAL_BOT_API_URL: str = os.getenv("LOCAL_BOT_API_URL", "http://localhost:8081")
 
 settings = Settings()
